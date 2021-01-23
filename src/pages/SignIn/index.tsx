d3d02e4b5bb8441558111d7b57fd7bf4b81ff9b5
Peterson Medina <opetersonmedina@outlook.com>
2021-01-23 14:13:51 -0300

    App Gobarber

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça Seu Logon</Title>
+
+      <Input name="email" icon="email" placeholder="Email" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('ClicouEnrar');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
